6a4eaee9fc62c5ed0d82212a545cfa01807c4645
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-02-14 13:16:54 +1100

    Update to rspec 3 syntax

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -5,15 +5,15 @@ require_relative './../enumerable_fun'
 
 describe "enumerable" do
   it "responds to cool methods" do
-    Array.new.respond_to?(:injekt).should be_true    
-    Hash.new.respond_to?(:injekt).should be_true    
+    expect(Array.new.respond_to?(:injekt)).to be_truthy
+    expect(Hash.new.respond_to?(:injekt)).to be_truthy
   end
   
   it "matches the default implementation of inject" do 
     a = [1,2,3].inject([]) {|acc,i| acc << i+i }
     b = [1,2,3].injekt([]) {|acc,i| acc << i+i }
-   # a.should eq([1,4,9]) 
-    b.should eq(a)
+
+    expect(b).to eq(a)
   end
 
 
@@ -21,7 +21,7 @@ describe "enumerable" do
     actual = [1,2,3].chart { |x| x+x }
     expect = [1,2,3].map { |x| x+x }
 
-    actual.should eq( expect)
+    expect(actual).to eq( expect)
   end
 
 
@@ -29,30 +29,30 @@ describe "enumerable" do
     bools = [true,false].enny? {|x|x}
     neg_bools = [false,false].enny? {|x|x}
     ints = [1,2,3].enny? {|x| x > 2 }
-    bools.should be_true
-    neg_bools.should be_false
-    ints.should be_true
+    expect(bools).to be_truthy
+    expect(neg_bools).to be_falsey
+    expect(ints).to be_truthy
   end
 
   it "does an enny? - sans block" do 
     bools = [true,false].enny?
-    bools.should be_true
+    expect(bools).to be_truthy
   end
 
   it "tells us everything" do 
     same = [true,true].everything? {|x|x} 
-    same.should be_true
+    expect(same).to be_truthy
 
     diffs = [true,false].everything? {|x|x}
-    diffs.should be_false
+    expect(diffs).to be_falsey
 
     ints = [1,2,3,4].everything? {|x| x < 5}
-    ints.should be_true
+    expect(ints).to be_truthy
   end
 
   it "tells us everything - sans block" do 
     same = [true,true].everything?
-    same.should be_true
+    expect(same).to be_truthy
   end
 
 
